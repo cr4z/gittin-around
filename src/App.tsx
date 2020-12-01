1cefa95b319e35a2bffa846f5a1e66e03fb31bac
steven <stevencr7zz@gmail.com>
2020-12-01 11:55:17 -0600

    Add pointless wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import { Chatbox } from "./components/Chatbox";
 
 function App() {
   return (
-    <div className="App">
-      <div className="top">gittin' around</div>
-      <Chatbox />
+    <div className="pointless-wrapper">
+      <div className="App">
+        <div className="top">gittin' around</div>
+        <Chatbox />
+      </div>
     </div>
   );
 }
