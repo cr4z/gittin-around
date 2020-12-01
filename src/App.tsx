42d4dcffa5519b29ec785c94414b8b5912aa03eb
steven <stevencr7zz@gmail.com>
2020-12-01 11:30:22 -0600

    Add chatbox to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Chatbox } from './components/Chatbox';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         git
       </header>
+      <Chatbox />
     </div>
   );
 }
